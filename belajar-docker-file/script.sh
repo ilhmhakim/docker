bf91596498050f73867959dc53b601e0e58fc14c
Ilham Hakim <ilhmhakim@gmail.com>
2024-11-20 11:53:01 +0700

    belajar dockerfile

--- belajar-docker-file/script.sh
+++ belajar-docker-file/script.sh
@@ -35,3 +35,24 @@ docker build -t kafta/copy copy
 docker container create --name copy kafta/copy
 docker container start copy
 docker container logs copy
+
+# .dockerignore
+docker build -t kafta/ignore ignore
+docker container create --name ignore kafta/ignore
+docker container start ignore
+docker container logs ignore
+
+# EXPOSE Instruction
+docker build -t kafta/expose expose
+docker image inspect kafta/expose
+docker container create --name expose -p 8081:8080 kafta/expose
+docker container start expose
+docker container logs expose
+
+# ENV Instruction
+docker build -t kafta/env env
+docker image inspect kafta/env
+docker container create --name env --env APP_PORT=9090 -p 9090:9090 kafta/env
+docker container start env
+docker container logs env
+docker container stop env
